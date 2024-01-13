8c04e336f37feacb62074c13fe03052cf32640e2
Rodrigo Nóbrega <rofen1@protonmail.com>
2024-01-13 19:23:16 -0300

    Buscar usuario pelo id

--- src/main/java/com/rodrigofnobrega/demoparkapi/service/UserService.java
+++ src/main/java/com/rodrigofnobrega/demoparkapi/service/UserService.java
@@ -3,8 +3,11 @@ package com.rodrigofnobrega.demoparkapi.service;
 import com.rodrigofnobrega.demoparkapi.entity.UserEntity;
 import com.rodrigofnobrega.demoparkapi.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.apache.catalina.User;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
@@ -14,5 +17,10 @@ public class UserService {
     @Transactional
     public UserEntity save(UserEntity user) {
         return userRepository.save(user);
+    }
+
+    @Transactional(readOnly = true)
+    public Optional<UserEntity> getById(Long id) {
+        return userRepository.findById(id);
     }
 }
--- src/main/java/com/rodrigofnobrega/demoparkapi/web/controller/UserController.java
+++ src/main/java/com/rodrigofnobrega/demoparkapi/web/controller/UserController.java
@@ -5,10 +5,10 @@ import com.rodrigofnobrega.demoparkapi.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Objects;
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @RestController
@@ -21,5 +21,17 @@ public class UserController {
         UserEntity userSaved = userService.save(user);
 
         return ResponseEntity.status(HttpStatus.CREATED).body(user);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Object> getById(@PathVariable Long id) {
+        Optional<UserEntity> user  = userService.getById(id);
+
+        if (!user.isPresent()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Usuário não encontrado");
+
+        }
+
+        return  ResponseEntity.ok(user.get());
     }
 }

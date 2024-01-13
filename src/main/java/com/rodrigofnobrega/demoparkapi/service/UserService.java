bcee96be2bc442eec86addb0eb20e69765aa4844
Rodrigo Nóbrega <rofen1@protonmail.com>
2024-01-13 17:46:41 -0300

    Create method to save user

--- src/main/java/com/rodrigofnobrega/demoparkapi/service/UserService.java
+++ src/main/java/com/rodrigofnobrega/demoparkapi/service/UserService.java
@@ -1,11 +1,18 @@
 package com.rodrigofnobrega.demoparkapi.service;
 
+import com.rodrigofnobrega.demoparkapi.entity.UserEntity;
 import com.rodrigofnobrega.demoparkapi.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @RequiredArgsConstructor
 @Service
 public class UserService {
     private final UserRepository userRepository;
+
+    @Transactional
+    public UserEntity save(UserEntity user) {
+        return userRepository.save(user);
+    }
 }
--- src/main/java/com/rodrigofnobrega/demoparkapi/web/controller/UserController.java
+++ src/main/java/com/rodrigofnobrega/demoparkapi/web/controller/UserController.java
@@ -1,7 +1,12 @@
 package com.rodrigofnobrega.demoparkapi.web.controller;
 
+import com.rodrigofnobrega.demoparkapi.entity.UserEntity;
 import com.rodrigofnobrega.demoparkapi.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,4 +15,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("api/v1/usuarios")
 public class UserController {
     private final UserService userService;
+
+    @PostMapping
+    public ResponseEntity<UserEntity> createUser(@RequestBody UserEntity user) {
+        UserEntity userSaved = userService.save(user);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(user);
+    }
 }

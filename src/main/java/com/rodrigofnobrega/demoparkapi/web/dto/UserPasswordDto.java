d64a6e1e249439ff11faea67c73a88636c4070b4
Rodrigo Nóbrega <rofen1@protonmail.com>
2024-01-14 17:20:56 -0300

    Validando o objeto UserPasswordDto

--- src/main/java/com/rodrigofnobrega/demoparkapi/web/dto/UserPasswordDto.java
+++ src/main/java/com/rodrigofnobrega/demoparkapi/web/dto/UserPasswordDto.java
@@ -1,10 +1,18 @@
 package com.rodrigofnobrega.demoparkapi.web.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 
 @Getter @Setter @NoArgsConstructor @AllArgsConstructor @ToString
 public class UserPasswordDto {
+    @NotBlank
+    @Size(min = 6, max = 6)
     private String currentPassword;
+    @NotBlank
+    @Size(min = 6, max = 6)
     private String newPassword;
+    @NotBlank
+    @Size(min = 6, max = 6)
     private String confirmPassword;
 }

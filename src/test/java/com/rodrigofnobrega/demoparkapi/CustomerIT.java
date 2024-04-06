ed528a8276224391603df51cfab48aa87b7370fe
Rodrigo Nóbrega <rofen1@protonmail.com>
2024-04-06 14:19:46 -0300

    Teste ponto a ponto para Create (Status 403, 409 e 422)

--- src/test/java/com/rodrigofnobrega/demoparkapi/CustomerIT.java
+++ src/test/java/com/rodrigofnobrega/demoparkapi/CustomerIT.java
@@ -2,6 +2,7 @@ package com.rodrigofnobrega.demoparkapi;
 
 import com.rodrigofnobrega.demoparkapi.web.dto.customer.CustomerCreateDto;
 import com.rodrigofnobrega.demoparkapi.web.dto.customer.CustomerResponseDto;
+import com.rodrigofnobrega.demoparkapi.web.exception.ErrorMessage;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,9 +16,6 @@ import org.springframework.test.web.reactive.server.WebTestClient;
 @Sql(scripts = "/sql/customers/customers-insert.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
 @Sql(scripts = "/sql/customers/customers-delete.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
 public class CustomerIT {
-//    @Autowired
-//    private WebTestClient testClient;
-
     @LocalServerPort
     private int port = 8080;
 
@@ -40,5 +38,90 @@ public class CustomerIT {
         Assertions.assertThat(responseBody.getId()).isNotNull();
         Assertions.assertThat(responseBody.getName()).isEqualTo("Tobias Ferreira");
         Assertions.assertThat(responseBody.getCpf()).isEqualTo("09961838092");
+    }
+
+    @Test
+    public void crateCustomer_WithCpfAlreadyRegistered_ReturnErrorMessage409() {
+        WebTestClient testClient = WebTestClient.bindToServer().baseUrl("http://127.0.0.1:" + port).build();
+
+        ErrorMessage responseBody = testClient
+                .post()
+                .uri("/api/v1/customers")
+                .contentType(MediaType.APPLICATION_JSON)
+                .headers(JwtAuthentication.getHeaderAuthorization(testClient, "toby@email.com", "123456"))
+                .bodyValue(new CustomerCreateDto("Tobias Ferreira", "26009338085"))
+                .exchange()
+                .expectStatus().isEqualTo(409)
+                .expectBody(ErrorMessage.class)
+                .returnResult().getResponseBody();
+
+        Assertions.assertThat(responseBody).isNotNull();
+        Assertions.assertThat(responseBody.getStatus()).isEqualTo(409);
+    }
+
+    @Test
+    public void crateCustomer_WithInvalidDatas_ReturnErrorMessage422() {
+        WebTestClient testClient = WebTestClient.bindToServer().baseUrl("http://127.0.0.1:" + port).build();
+
+        ErrorMessage responseBody = testClient
+                .post()
+                .uri("/api/v1/customers")
+                .contentType(MediaType.APPLICATION_JSON)
+                .headers(JwtAuthentication.getHeaderAuthorization(testClient, "toby@email.com", "123456"))
+                .bodyValue(new CustomerCreateDto("", ""))
+                .exchange()
+                .expectStatus().isEqualTo(422)
+                .expectBody(ErrorMessage.class)
+                .returnResult().getResponseBody();
+
+        Assertions.assertThat(responseBody).isNotNull();
+        Assertions.assertThat(responseBody.getStatus()).isEqualTo(422);
+
+        responseBody = testClient
+                .post()
+                .uri("/api/v1/customers")
+                .contentType(MediaType.APPLICATION_JSON)
+                .headers(JwtAuthentication.getHeaderAuthorization(testClient, "toby@email.com", "123456"))
+                .bodyValue(new CustomerCreateDto("Bobb", "00000000000"))
+                .exchange()
+                .expectStatus().isEqualTo(422)
+                .expectBody(ErrorMessage.class)
+                .returnResult().getResponseBody();
+
+        Assertions.assertThat(responseBody).isNotNull();
+        Assertions.assertThat(responseBody.getStatus()).isEqualTo(422);
+
+        responseBody = testClient
+                .post()
+                .uri("/api/v1/customers")
+                .contentType(MediaType.APPLICATION_JSON)
+                .headers(JwtAuthentication.getHeaderAuthorization(testClient, "toby@email.com", "123456"))
+                .bodyValue(new CustomerCreateDto("Bobb", "099.618.380-92"))
+                .exchange()
+                .expectStatus().isEqualTo(422)
+                .expectBody(ErrorMessage.class)
+                .returnResult().getResponseBody();
+
+        Assertions.assertThat(responseBody).isNotNull();
+        Assertions.assertThat(responseBody.getStatus()).isEqualTo(422);
+    }
+
+    @Test
+    public void crateCustomer_WithUserNotAllowed_ReturnErrorMessage403() {
+        WebTestClient testClient = WebTestClient.bindToServer().baseUrl("http://127.0.0.1:" + port).build();
+
+        ErrorMessage responseBody = testClient
+                .post()
+                .uri("/api/v1/customers")
+                .contentType(MediaType.APPLICATION_JSON)
+                .headers(JwtAuthentication.getHeaderAuthorization(testClient, "ana@email.com", "123456"))
+                .bodyValue(new CustomerCreateDto("Tobias Ferreira", "09961838092"))
+                .exchange()
+                .expectStatus().isForbidden()
+                .expectBody(ErrorMessage.class)
+                .returnResult().getResponseBody();
+
+        Assertions.assertThat(responseBody).isNotNull();
+        Assertions.assertThat(responseBody.getStatus()).isEqualTo(403);
     }
 }
